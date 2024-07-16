fe4892ab772eb512487b286121264f924d9a6c2f
elliot-hacks <thisguyhack@gmail.com>
2024-07-16 20:54:16 +0300

    implement fingerprint scan

--- home/mantra_sdk.py
+++ home/mantra_sdk.py
@@ -1,88 +1,93 @@
 import requests
+import json
 
 class MantraSDK:
-    def __init__(self, base_url="https://localhost:8003/mfs100/"):
-        self.base_url = base_url
+    def __init__(self, uri="https://localhost:8003/mfs100/"):
+        self.uri = uri
+
+    def _post(self, method, data):
+        url = self.uri + method
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        try:
+            response = requests.post(url, headers=headers, data=json.dumps(data))
+            response.raise_for_status()
+            return {'httpStatus': True, 'data': response.json()}
+        except requests.exceptions.HTTPError as errh:
+            return {'httpStatus': False, 'err': self._get_http_error(response.status_code)}
+        except requests.exceptions.RequestException as err:
+            return {'httpStatus': False, 'err': str(err)}
+
+    def _get(self, method):
+        url = self.uri + method
+        headers = {'Content-Type': 'application/json; charset=utf-8'}
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            return {'httpStatus': True, 'data': response.json()}
+        except requests.exceptions.HTTPError as errh:
+            return {'httpStatus': False, 'err': self._get_http_error(response.status_code)}
+        except requests.exceptions.RequestException as err:
+            return {'httpStatus': False, 'err': str(err)}
+
+    def _get_http_error(self, status_code):
+        if status_code == 0:
+            return 'Service Unavailable'
+        elif status_code == 404:
+            return 'Requested page not found'
+        elif status_code == 500:
+            return 'Internal Server Error'
+        else:
+            return 'Unhandled Error'
 
     def get_mfs100_info(self):
         return self._get("info")
 
     def get_mfs100_key_info(self, key):
-        data = {
-            "Key": key
-        }
+        data = {"Key": key}
         return self._post("keyinfo", data)
 
     def capture_finger(self, quality, timeout):
-        data = {
-            "Quality": quality,
-            "TimeOut": timeout
-        }
+        data = {"Quality": quality, "TimeOut": timeout}
         return self._post("capture", data)
 
-    def capture_multi_finger(self, quality, timeout, no_of_fingers):
-        data = {
-            "Quality": quality,
-            "TimeOut": timeout,
-            "NoOfFinger": no_of_fingers
-        }
+    def capture_multi_finger(self, quality, timeout, no_of_finger):
+        data = {"Quality": quality, "TimeOut": timeout, "NoOfFinger": no_of_finger}
         return self._post("capturewithdeduplicate", data)
 
-    def verify_finger(self, prob_template, gallery_template):
+    def verify_finger(self, prob_fmr, gallery_fmr):
         data = {
-            "ProbTemplate": prob_template,
-            "GalleryTemplate": gallery_template,
+            "ProbTemplate": prob_fmr,
+            "GalleryTemplate": gallery_fmr,
             "BioType": "FMR"
         }
         return self._post("verify", data)
 
-    def match_finger(self, quality, timeout, gallery_template):
+    def match_finger(self, quality, timeout, gallery_fmr):
         data = {
             "Quality": quality,
             "TimeOut": timeout,
-            "GalleryTemplate": gallery_template,
+            "GalleryTemplate": gallery_fmr,
             "BioType": "FMR"
         }
         return self._post("match", data)
 
     def get_pid_data(self, biometric_array):
-        data = {
-            "Biometrics": biometric_array
-        }
+        data = {"Biometrics": biometric_array}
         return self._post("getpiddata", data)
 
     def get_rbd_data(self, biometric_array):
-        data = {
-            "Biometrics": biometric_array
-        }
+        data = {"Biometrics": biometric_array}
         return self._post("getrbddata", data)
 
-    def _post(self, method, data):
-        url = self.base_url + method
-        headers = {
-            "Content-Type": "application/json"
-        }
-        response = requests.post(url, json=data, headers=headers, verify=False)
-        if response.status_code == 200:
-            return {"httpStatus": True, "data": response.json()}
-        else:
-            return {"httpStatus": False, "err": self._get_http_error(response)}
+# Classes to represent Biometric data
+class Biometric:
+    def __init__(self, bio_type, biometric_data, pos, nfiq, na):
+        self.BioType = bio_type
+        self.BiometricData = biometric_data
+        self.Pos = pos
+        self.Nfiq = nfiq
+        self.Na = na
 
-    def _get(self, method):
-        url = self.base_url + method
-        response = requests.get(url, verify=False)
-        if response.status_code == 200:
-            return {"httpStatus": True, "data": response.json()}
-        else:
-            return {"httpStatus": False, "err": self._get_http_error(response)}
-
-    def _get_http_error(self, response):
-        if response.status_code == 0:
-            return 'Service Unavailable'
-        elif response.status_code == 404:
-            return 'Requested page not found'
-        elif response.status_code == 500:
-            return 'Internal Server Error'
-        else:
-            return f"Unhandled Error: {response.status_code}"
-
+class MFS100Request:
+    def __init__(self, biometric_array):
+        self.Biometrics = biometric_array
--- home/views.py
+++ home/views.py
@@ -1,80 +1,132 @@
-# views.py
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.utils import timezone
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-from .forms import RegisterForm
-from .models import Register, Login
-from .utils import encrypt
-import hashlib
+from .mantra_sdk import MantraSDK
 from .models import UserProfile
 
-@login_required
-def user_login(request):
-    if request.method == 'POST' and 'submit' in request.POST:
-        fingerprint_matched = request.POST.get('fingerprint_matched', '0')
-        if fingerprint_matched == '1':
-            return redirect('next_page_url')  # Change 'next_page_url' to the appropriate URL
+# Initialize Mantra SDK
+sdk = MantraSDK()
 
-    user_profile = UserProfile.objects.get(user=request.user)
+@login_required
+def fingerprint_scan(request):
+    user = request.user
+    user_profile = UserProfile.objects.get(user=user)
     context = {
-        'fingerprint': user_profile.fingerprint,
+        'fingerprint_template': user_profile.fingerprint_template
     }
-    return render(request, 'user_login.html', context)
+    return render(request, 'fingerprint_scan.html', context)
 
 @login_required
-def get_fingerprint(request):
-    user_profile = UserProfile.objects.get(user=request.user)
-    return JsonResponse({'fingerprint': user_profile.fingerprint})
-
-
-def register_view(request):
+def match_fingerprint(request):
     if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email_id']
-            password = form.cleaned_data['password']
-            confirm_password = form.cleaned_data['exampleInputPassword2']
-            fingerprint = form.cleaned_data['fingerprint']
+        quality = request.POST.get('quality', 60)
+        timeout = request.POST.get('timeout', 10)
+        fingerprint_template = request.POST.get('fingerprint_template')
 
-            if Register.objects.filter(email_id=email).exists():
-                messages.error(request, "Email already exists.")
+        response = sdk.match_finger(quality, timeout, fingerprint_template)
+        
+        if response['httpStatus']:
+            if response['data']['Status']:
+                return JsonResponse({'status': 'success', 'message': 'Fingerprint matched'})
             else:
-                hashed_password = hashlib.md5(password.encode()).hexdigest()
-                ip = request.META.get('REMOTE_ADDR')
-                my_date = timezone.now()
-
-                register = Register(
-                    email_id=email,
-                    password=hashed_password,
-                    confirm_password=hashed_password,
-                    activation=True,
-                    ip=ip,
-                    date=my_date,
-                    fingerprint=fingerprint
-                )
-                register.save()
-
-                login = Login(
-                    email_id=email,
-                    password=hashed_password,
-                    activation=True
-                )
-                login.save()
-
-                messages.success(request, "Successfully Registered!")
-                return redirect('login')  # Redirect to login page after successful registration
+                return JsonResponse({'status': 'failure', 'message': 'Fingerprint not matched', 'error': response['data']['ErrorDescription']})
         else:
-            messages.error(request, "Form is not valid. Please check the input.")
-    else:
-        form = RegisterForm()
+            return JsonResponse({'status': 'error', 'message': 'HTTP request failed', 'error': response['err']})
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
-    return render(request, 'register.html', {'form': form})
+@login_required
+def capture_fingerprint(request):
+    if request.method == "POST":
+        quality = request.POST.get('quality', 60)
+        timeout = request.POST.get('timeout', 10)
+
+        response = sdk.capture_finger(quality, timeout)
+        
+        if response['httpStatus']:
+            return JsonResponse({'status': 'success', 'data': response['data']})
+        else:
+            return JsonResponse({'status': 'error', 'message': 'HTTP request failed', 'error': response['err']})
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
 
-def remove_user_and_redirect(request):
-    User.objects.filter(username=request.user.username).delete()
-    logout(request)
-    messages.error(request, f'Your fingerprint capture was not valid. Kindly re-register and try again.')
-    return redirect(reverse('accounts:register'))
+
+# # views.py
+# from django.shortcuts import render, redirect
+# from django.contrib import messages
+# from django.utils import timezone
+# from django.http import JsonResponse
+# from django.contrib.auth.decorators import login_required
+# from .forms import RegisterForm
+# from .models import Register, Login
+# from .utils import encrypt
+# import hashlib
+# from .models import UserProfile
+
+# @login_required
+# def user_login(request):
+#     if request.method == 'POST' and 'submit' in request.POST:
+#         fingerprint_matched = request.POST.get('fingerprint_matched', '0')
+#         if fingerprint_matched == '1':
+#             return redirect('next_page_url')  # Change 'next_page_url' to the appropriate URL
+
+#     user_profile = UserProfile.objects.get(user=request.user)
+#     context = {
+#         'fingerprint': user_profile.fingerprint,
+#     }
+#     return render(request, 'user_login.html', context)
+
+# @login_required
+# def get_fingerprint(request):
+#     user_profile = UserProfile.objects.get(user=request.user)
+#     return JsonResponse({'fingerprint': user_profile.fingerprint})
+
+
+# def register_view(request):
+#     if request.method == "POST":
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data['email_id']
+#             password = form.cleaned_data['password']
+#             confirm_password = form.cleaned_data['exampleInputPassword2']
+#             fingerprint = form.cleaned_data['fingerprint']
+
+#             if Register.objects.filter(email_id=email).exists():
+#                 messages.error(request, "Email already exists.")
+#             else:
+#                 hashed_password = hashlib.md5(password.encode()).hexdigest()
+#                 ip = request.META.get('REMOTE_ADDR')
+#                 my_date = timezone.now()
+
+#                 register = Register(
+#                     email_id=email,
+#                     password=hashed_password,
+#                     confirm_password=hashed_password,
+#                     activation=True,
+#                     ip=ip,
+#                     date=my_date,
+#                     fingerprint=fingerprint
+#                 )
+#                 register.save()
+
+#                 login = Login(
+#                     email_id=email,
+#                     password=hashed_password,
+#                     activation=True
+#                 )
+#                 login.save()
+
+#                 messages.success(request, "Successfully Registered!")
+#                 return redirect('login')  # Redirect to login page after successful registration
+#         else:
+#             messages.error(request, "Form is not valid. Please check the input.")
+#     else:
+#         form = RegisterForm()
+
+#     return render(request, 'register.html', {'form': form})
+
+
+# def remove_user_and_redirect(request):
+#     User.objects.filter(username=request.user.username).delete()
+#     logout(request)
+#     messages.error(request, f'Your fingerprint capture was not valid. Kindly re-register and try again.')
+#     return redirect(reverse('accounts:register'))
